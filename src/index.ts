a1d58718d840e3bea7422e94a76aefd64b68e0a9
Mish Ushakov <mishushakov@users.noreply.github.com>
2022-10-20 20:35:43 +0200

    Catching errors originating from grpc-js package

--- src/index.ts
+++ src/index.ts
@@ -40,45 +40,49 @@ export type gRPCResponse = {
 
 export async function makeRequest (proto: string, { beforeRequest, afterResponse, ...clientConfig }: ClientConfig): Promise<gRPCResponse> {
   return new Promise(async (resolve, reject) => {
-    const root = await protobuf.load(proto)
-    const [packageName, serviceName] = clientConfig.service.split('.')
+    try {
+      const root = await protobuf.load(proto)
+      const [packageName, serviceName] = clientConfig.service.split('.')
 
-    const { requestType, responseType } = root.lookup(`${packageName}.${clientConfig.method}`) as unknown as LookupResult
-    const requestMessageType = root.lookupType(requestType)
-    const responseMessageType = root.lookupType(responseType)
+      const { requestType, responseType } = root.lookup(`${packageName}.${clientConfig.method}`) as unknown as LookupResult
+      const requestMessageType = root.lookupType(requestType)
+      const responseMessageType = root.lookupType(responseType)
 
-    const verifyError = requestMessageType.verify(clientConfig.data)
-    if (verifyError) return reject(Error(verifyError))
+      const verifyError = requestMessageType.verify(clientConfig.data)
+      if (verifyError) return reject(Error(verifyError))
 
-    const message = requestMessageType.create(clientConfig.data)
-    const messageEncoded = requestMessageType.encode(message).finish()
+      const message = requestMessageType.create(clientConfig.data)
+      const messageEncoded = requestMessageType.encode(message).finish()
 
-    let credentials
-    if (!clientConfig.tls) {
-      credentials = grpc.credentials.createInsecure()
-    } else {
-      credentials = grpc.credentials.createSsl(
-        clientConfig.tls.rootCerts ? Buffer.from(clientConfig.tls.rootCerts) : undefined,
-        clientConfig.tls.privateKey ? Buffer.from(clientConfig.tls.privateKey) : undefined,
-        clientConfig.tls.certChain ? Buffer.from(clientConfig.tls.certChain) : undefined,
-        clientConfig. tls.verifyOptions
-      )
-    }
-
-    const client = new grpc.Client(clientConfig.host, credentials)
-    if (beforeRequest) beforeRequest(clientConfig)
-
-    client.makeUnaryRequest(`/${packageName}.${serviceName}/${clientConfig.method}`, x => x, x => x, Buffer.from(messageEncoded), (error, message) => {
-      if (error) return reject(error)
-      if (message) {
-        const response = {
-          message: responseMessageType.decode(message).toJSON(),
-          size: message.byteLength
-        }
-
-        if (afterResponse) afterResponse(response)
-        return resolve(response)
+      let credentials
+      if (!clientConfig.tls) {
+        credentials = grpc.credentials.createInsecure()
+      } else {
+        credentials = grpc.credentials.createSsl(
+          clientConfig.tls.rootCerts ? Buffer.from(clientConfig.tls.rootCerts) : undefined,
+          clientConfig.tls.privateKey ? Buffer.from(clientConfig.tls.privateKey) : undefined,
+          clientConfig.tls.certChain ? Buffer.from(clientConfig.tls.certChain) : undefined,
+          clientConfig.tls.verifyOptions
+        )
       }
-    })
+
+      const client = new grpc.Client(clientConfig.host, credentials)
+      if (beforeRequest) beforeRequest(clientConfig)
+
+      client.makeUnaryRequest(`/${packageName}.${serviceName}/${clientConfig.method}`, x => x, x => x, Buffer.from(messageEncoded), (error, message) => {
+        if (error) return reject(error)
+        if (message) {
+          const response = {
+            message: responseMessageType.decode(message).toJSON(),
+            size: message.byteLength
+          }
+
+          if (afterResponse) afterResponse(response)
+          return resolve(response)
+        }
+      })
+    } catch (e) {
+      reject(e)
+    }
   })
 }
