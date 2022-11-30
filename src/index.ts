a4b3facb1ad10dfeaa457c2f63d768ca257816fb
Mish Ushakov <mishushakov@users.noreply.github.com>
2022-11-30 22:21:05 +0100

    Closes #11

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ export type gRPCRequestMetadata = {
 }
 
 export type gRPCRequestTLS = {
-  rootCerts?: string
-  privateKey?: string
-  certChain?: string
+  rootCerts?: string | Buffer
+  privateKey?: string | Buffer
+  certChain?: string | Buffer
   verifyOptions?: VerifyOptions
 }
 
@@ -60,9 +60,21 @@ export async function makeRequest (proto: string | string[], { beforeRequest, af
       let credentials
       if (clientConfig.tls && Object.keys(clientConfig.tls).length !== 0) {
         credentials = grpc.credentials.createSsl(
-          clientConfig.tls.rootCerts ? Buffer.from(clientConfig.tls.rootCerts) : undefined,
-          clientConfig.tls.privateKey ? Buffer.from(clientConfig.tls.privateKey) : undefined,
-          clientConfig.tls.certChain ? Buffer.from(clientConfig.tls.certChain) : undefined,
+          clientConfig.tls.rootCerts
+            ? Buffer.isBuffer(clientConfig.tls.rootCerts)
+              ? clientConfig.tls.rootCerts
+              : Buffer.from(clientConfig.tls.rootCerts)
+            : undefined,
+          clientConfig.tls.privateKey
+            ? Buffer.isBuffer(clientConfig.tls.privateKey)
+              ? clientConfig.tls.privateKey
+              : Buffer.from(clientConfig.tls.privateKey)
+            : undefined,
+          clientConfig.tls.certChain
+            ? Buffer.isBuffer(clientConfig.tls.certChain)
+              ? clientConfig.tls.certChain
+              : Buffer.from(clientConfig.tls.certChain)
+            : undefined,
           clientConfig.tls.verifyOptions
         )
       } else {
